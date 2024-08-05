7399212e91ba1458b6508ce8487e1c6069c8d2d2
demid <demid@localhost.local>
2024-08-05 21:51:24 +0300

    .

--- collect_logs.sh
+++ collect_logs.sh
@@ -11,7 +11,8 @@ function collect-logs ()
     do 
       out+="$i "
     done
-
+    
+    out="${out::-1}"
     printf "\033[91m""$out""\033[0m"
   }
   
@@ -23,9 +24,25 @@ function collect-logs ()
     do 
       out+="$i "
     done
-
+    
+    out="${out::-1}"
     printf "\033[92m""$out""\033[0m"
   }
+
+
+  function YELLOW ()
+  {
+    out=""
+    for i in "$@"
+    do 
+      out+="$i "
+    done
+
+
+    out="${out::-1}"
+    printf "\033[93m""$out""\033[0m"
+  }
+  
 
   
   function remove ()
@@ -39,8 +56,9 @@ function collect-logs ()
   #Secure exit
   function my_exit ()
   {
-    cd "$START" 
+    cd "$START" 2>/dev/null 
     remove
+    sudo chown -R demid:users AMDZ_UNZIPPED_LOGS 2>/dev/null
     exit "$1"
   }        
 
@@ -56,7 +74,7 @@ function collect-logs ()
   {
     for file in `find "$1" -type f -name "*"`
     do
-      printf "$file \n"
+      printf "$(printf "$file" | sed 's/$1//' | sed 's/..//')\n"
     done
   }
   
@@ -82,11 +100,7 @@ function collect-logs ()
   }
   trap signal_continue SIGCONT  
   
-
-  #arch
-  BIOS="/usr/share/edk2/x64/OVMF.4m.fd"
-  #debian
-  #BIOS="/usr/share/OVMF/OVMF_CODE_4M.fd"
+   
   RAM_SIZE=1000M
   SMP=2
   DISK_SIZE=100M
@@ -99,7 +113,7 @@ function collect-logs ()
   Arguments:
       --help,-h,help    - show this help
       -f                - choose verifiable .efi file 
-      -b                - choose bios version
+      -b (optional)     - choose bios version
       -m (optional)     - choose RAM size (default is 1000M)
       -s (optional)     - choose number of cores (default is 2)
       -d (optional)     - choose disk space (default is 100M)
@@ -131,18 +145,66 @@ function collect-logs ()
     case "${opt}" in
           b)
               BIOS="$OPTARG"
+
+              if (( $(echo "$BIOS" | grep .4m.fd | grep OVMF | wc -l) != 1 ))
+              then 
+                printf "Incorrect -b option\n"
+                exit 1
+              fi
+
               ;;
           m)
               RAM_SIZE="$OPTARG"
+              if (( $(echo "$RAM_SIZE" | grep M | wc -l) != 1 )) && \
+                  (( $(echo "$RAM_SIZE" | grep G | wc -l) != 1 ))
+              then
+                printf "Incorrect -m option\n"
+                exit 1 
+              fi 
+              
+
+              if ! [[ $(echo "$RAM_SIZE" | sed 's/M//' | sed 's/G//') =~ ^-?[0-9]+$ ]]
+              then
+                printf "Incorrect -m option\n"
+                exit 1
+              fi
               ;;
           s)
               SMP="$OPTARG"
+
+              if ! [[ "$SMP" =~ ^-?[0-9]+$ ]]
+              then
+                printf "Incorrect -s option\n"
+                exit 1
+              fi
+
               ;; 
           d)
               DISK_SIZE="$OPTARG"
+
+              if (( $(echo "$DISK_SIZE" | grep M | wc -l) != 1 )) && \
+                  (( $(echo "$DISK_SIZE" | grep G | wc -l) != 1 ))
+              then
+                printf "Incorrect -d option\n"
+                exit 1 
+              fi 
+              
+
+              if ! [[ $(echo "$DISK_SIZE" | sed 's/M//' | sed 's/G//') =~ ^-?[0-9]+$ ]]
+              then
+                printf "Incorrect -d option\n"
+                exit 1
+              fi
+
               ;;
           f)
               MACHINE="$OPTARG"
+
+              if (( $(echo "$MACHINE" | grep .efi | wc -l) != 1 ))
+              then 
+                printf "Incorrect -f option\n"
+                exit 1
+              fi
               ;;
           *)
               echo "Incorrect options. Try -h to see help page"
@@ -150,7 +212,30 @@ function collect-logs ()
               ;;
       esac
   done
-
+  
+  #Try to find bios in system if it wasn't set
+  if [ -z "$BIOS" ]
+  then
+    BIOS_PATH1="/usr/share/edk2/x64/OVMF.4m.fd"
+    BIOS_PATH2="/usr/share/OVMF/x64/OVMF.4m.fd"
+  
+    if [ -f "$BIOS_PATH1" ]
+    then
+      BIOS="$BIOS_PATH1"
+    elif [ -f "$BIOS_PATH2" ]
+    then
+      BIOS="$BIOS_PATH2"
+    else
+      WHERE_IS_OVMF=$(whereis OVMF | sed 's/OVMF: //')
+      BIOS=$(recursive_ls "$WHERE_IS_OVMF" | grep OVMF.4m.fd)
+    fi
+  fi 
+  
+  if [ -z "$BIOS" ]
+  then
+    RED "BIOS wasn't set by user and wasn't found by script.\n"
+    my_exit 1
+  fi
 
   #CREATING VFAT GPT DISK
   dd if=/dev/zero of=disk.img bs="$DISK_SIZE" count=1 status=progress >> /dev/null 2>&1
@@ -191,159 +276,257 @@ function collect-logs ()
     -monitor none                  \
     2>/dev/null
   
-  printf "\n"
+  printf "                                                \n"
   mount -t vfat disk.img disk
-  ls disk/AMDZ_HW_LOG
-
-  #UNZIPPING ARCHIVE IN "AMDZ_UNZIPPED_LOGS/MACHINE_NAME/TEST+DATE/" DIRECTORY
-  ARCHIVE="$(ls "disk/AMDZ_HW_LOG" | sort | grep log___ | grep .tar.xvf | tail -n 1)"
-  ARCHIVE_PATH="disk/AMDZ_HW_LOG""$ARCHIVE"
-
-  mkdir -p "AMDZ_UNZIPPED_LOGS"
-  CURRENT_MACHINE_DIR=$(echo "AMDZ_UNZIPPED_LOGS/""$MACHINE" | sed 's/.efi//')
-  mkdir -p "$CURRENT_MACHINE_DIR"
-
-  TEST_NAME=$(echo "$ARCHIVE" | sed 's/log___/test/')
-  CURRENT_TEST_DIR="$CURRENT_MACHINE_DIR/""$TEST_NAME/"  
-  mkdir "$CURRENT_TEST_DIR" || { RED "$TEST_NAME already exists\n"; my_exit 1; }
-  mv "$ARCHIVE_PATH" "$CURRENT_TEST_DIR"
   
-  my_cd "$CURRENT_TEST_DIR"
+  #UNZIPPING ARCHIVE IN "AMDZ_UNZIPPED_LOGS/MACHINE_NAME/TEST+DATE/" DIRECTORY
+  ARCHIVE="$(ls "disk/AMDZ_HW_LOG/" | sort | grep log___ | grep .tar.gz | tail -n 1)"
+  ARCHIVE_PATH="disk/AMDZ_HW_LOG/""$ARCHIVE"
+
+  if [ -z "$ARCHIVE_PATH" ]
+  then 
+    RED "\nArchive wasn't found\n"
+    my_exit 1 
+  fi 
+  
+  mkdir -p "AMDZ_UNZIPPED_LOGS"
+  MACHINE_DIR=$(echo "AMDZ_UNZIPPED_LOGS/""$MACHINE" | sed 's/.efi//')
+  mkdir -p "$MACHINE_DIR"
+
+  TEST_NAME=$(echo "$ARCHIVE" | sed 's/log___/test/' | sed 's/.tar.gz//')
+  TEST_DIR="$MACHINE_DIR/""$TEST_NAME/"
+  
+  mkdir "$TEST_DIR" || { RED "$TEST_NAME already exists\n"; my_exit 1; }
+  mv "$ARCHIVE_PATH" "$TEST_DIR"
+
+  my_cd "$TEST_DIR"
   tar xvf "$ARCHIVE" 1> /dev/null
 
-  UNZIPPED_DIR=$(echo "$ARCHIVE" | sed 's/.tar.xvf//')
+  UNZIPPED_DIR=$(echo "$ARCHIVE" | sed 's/.tar.gz//')
   my_cd "$UNZIPPED_DIR"
-  
   
   #LOGS TO FIND
   CHECK_LIST=(blkid.list boot_files.list cmdline.log dmesg.log dmidecode.list \
     dmi-raw-sysfs.bin drivers.log fdisk.log fstab.list grub.cfg/@boot@grub@grub.cfg \
     hwinfo.log hw.log iomem.log ioports.log kernel_config.log lsblk.log lsb.log \
-    lsmod.log lspci.log lspcit.log lspcix.log mounts.log pcsc.log udevblock/nvme0n1.log \
-    udevblock/nvme0n1p1.log  udevblock/nvme0n1p2.log  udevblock/nvme0n1p3.log  \
-    udevblock/nvme0n1p4.log uname.log usb.log usbt.log version.log)
+    lsmod.log lspci.log lspcit.log lspcix.log mounts.log pcsc.log \
+    uname.log usb.log usbt.log version.log cmos_test.bin cmos_test.txt prefix.bin nvram_test.bin)
 
   
-  LOGS=($(Recursive_ls "$UNZIPPED_DIR"))
+  LOGS=($(recursive_ls .))
   OK_FILES=()
   NOT_EXISTING_FILES=()
   EMPTY_FILES=()
   UNEXPECTED_FILES=()
   
-
-  #Regular expr of array using for find
-  function reg_expr ()
+  
+  #finds substring in string
+  function my_find ()
   {
-    ARR=($1)
-    REG_EXP=($(printf '%s|' "${ARR[@]}")) 
-    LEN=${#REG_EXP}
-    let LEN-=1
-    REG_EXP=($(echo "$REG_EXP" | cut -c -"$LEN"))
-    REG_EXP='^('"$REG_EXP"')$'
+    ARRAY=($@)
+    let ARR_LEN="${#ARRAY[@]}"-1
+    ARRAY2="${ARRAY[@]:1:$ARR_LEN}"
+    FILE="\<${1}\>" 
+
+    if [[ ${ARRAY2[@]} =~ $FILE ]]
+    then
+      echo 1
+    else
+      echo 0
+    fi
   }
       
 
   #Find out if there are any not-found files
   for FILE in "${CHECK_LIST[@]}"
   do
-    if [[ "$FILE" =~ ($(reg_expr "${LOGS[@]}")) ]]; then
-      :
-    else
+    if (( "$(my_find "$FILE" "${LOGS[@]}")" == 0))
+    then
       NOT_FOUND_FILES+=("$FILE")
-      #printf "File ""$(RED "$FILE")"" not exists\n"
     fi 
   done
 
+  EFIVARS_CREATED=0
+  VARS_CREATED=0
+
+  EFIVARS_LEN=0 
+  VARS_LEN=0
+
+  ICLIBS_CREATED=0 
+  ICLIBS_LEN=0
+
+  UDEVBLOCK_CREATED=0 
+  UDEVBLOCK_LEN=0
+
+  ACPIDUMP_CREATED=0 
+  ACPIDUMP_LEN=0
+
   #Sort successed, empty and unexpected files
   for FILE in "${LOGS[@]}"
-  do
-    if [[ "$FILE" =~ ($(reg_expr "${CHECK_LIST[@]}")) ]]; then
-      if [[ -s "$FILE" ]]; then
+  do 
+    if (( $(echo "$FILE" | grep efivars/ | wc -l) == 1 ))
+    then
+      EFIVARS_CREATED=1
+      let EFIVARS_LEN+=1
+      continue 
+    fi
+    
+    if (( $(echo "$FILE" | grep vars/ | wc -l) == 1 ))
+    then
+      VARS_CREATED=1
+      let VARS_LEN+=1
+      continue 
+    fi 
+
+    if (( $(echo "$FILE" | grep ic-libs/ | wc -l) == 1 ))
+    then
+      ICLIBS_CREATED=1
+      let ICLIBS_LEN+=1
+      continue 
+    fi
+
+    if (( $(echo "$FILE" | grep udevblock/ | wc -l) == 1 ))
+    then
+      UDEVBLOCK_CREATED=1
+      let UDEVBLOCK_LEN+=1
+      continue 
+    fi
+
+    if (( $(echo "$FILE" | grep acpidump/ | wc -l) == 1 ))
+    then
+      ACPIDUMP_CREATED=1
+      let ACPIDUMP_LEN+=1
+      continue 
+    fi
+
+
+    if (( "$(my_find "$FILE" "${CHECK_LIST[@]}")" == 1 ))
+    then
+      if [[ -s "$FILE" ]]
+      then
         OK_FILES+=("$FILE")
-        #printf "File ""$(GREEN "$FILE")"" exists and not empty\n"
       else
         EMPTY_FILES+=("$FILE")
-        #printf "File ""$(RED "$FILE")"" is empty\n"
       fi
     else
       UNEXPECTED_FILES+=("$FILE")
     fi
   done
 
-  #Output results
-  printf "\n           LOG COLLECTION RESULTS:"
-  printf "$PIPE"
 
+  #Output results
+  YELLOW "\n           LOG COLLECTION RESULTS:"
+  printf "$PIPE"
+  YELLOW "Tested machine : "
+  printf "$MACHINE\n"
+  YELLOW "Date : "
+  printf "$(echo "$ARCHIVE" | sed 's/log___//' | sed 's/.tar.gz//')\n"
+
+  
+  LOGS_LEN="${#LOGS[@]}"
   CL_LEN="${#CHECK_LIST[@]}"
   OK_LEN="${#OK_FILES[@]}"
   NEF_LEN="${#NOT_FOUND_FILES[@]}"
   EMP_LEN="${#EMPTY_FILES[@]}"
-  UNEXP_LEN="${UNEXPECTED_FILES[@]}"
-  
+  UNEXP_LEN="${#UNEXPECTED_FILES[@]}"
+  NEEDED_LEN="$CL_LEN"
+
+  let OK_LEN+=$EFIVARS_LEN+$VARS_LEN+$ICLIBS_LEN+$ACPIDUMP_LEN+$UDEVBLOCK_LEN
   let FOUND_LEN=$OK_LEN+$EMP_LEN
+  let NEEDED_LEN+=$EFIVARS_LEN+$VARS_LEN+$ICLIBS_LEN+$ACPIDUMP_LEN+$UDEVBLOCK_LEN
   
-  if [ "$OK_LEN" != 0 ]; then
-    #GREEN "\nSUCCESSFUL ($OK_LEN file(s)):\n"
+  if [ "$OK_LEN" != 0 ]
+  then
+    GREEN "\nSUCCESSFUL ($OK_LEN file(s)):\n"
+    
+    if [[ "$EFIVARS_CREATED" == 1 ]]
+    then
+      printf "efivars/ ""($EFIVARS_LEN"" files)\n"
+    fi 
+
+    if [[ "$VARS_CREATED" == 1 ]]
+    then
+      printf "vars/ ""($VARS_LEN"" files)\n"
+    fi
+
+    if [[ "$ICLIBS_CREATED" == 1 ]]
+    then
+      printf "ic-libs/ ""($ICLIBS_LEN"" files)\n"
+    fi
+
+    if [[ "$ACPIDUMP_CREATED" == 1 ]]
+    then
+      printf "acpidump/ ""($ACPIDUMP_LEN"" files)\n"
+    fi
+
+    if [[ "$UDEVBLOCK_CREATED" == 1 ]]
+    then
+      printf "udevblock/ ""($UDEVBLOCK"" files)\n"
+    fi
+
 
     for FILE in "${OK_FILES[@]}"
     do 
-      printf "File "
-      GREEN "$FILE"
-      printf " exists and not empty\n"
+      printf "$FILE\n"
     done
   fi
 
-  if [[ "$NEF_LEN" != 0 ]] || [[ "$EMP_LEN" != 0 ]]; then
-    RED \
-    "#############
-     # BAD FILES:#
-     #############\n"
-  fi
   
-  if [ "$NEF_LEN" != 0 ]; then
-    #RED "\nNOT FOUND ($NEF_LEN file(s)):\n"
+  if [ "$NEF_LEN" != 0 ]
+  then
+    RED "\nNOT FOUND ($NEF_LEN file(s)):\n"
 
-    for FILE in "${NOT_EXISTING_FILES[@]}"
+    for FILE in "${NOT_FOUND_FILES[@]}"
     do 
-      printf "File "
-      RED "$FILE"
-      printf " not found\n"
+      printf "$FILE\n"
     done
   fi
 
-  if [ "$EMP_LEN" != 0 ]; then
-    #RED "\nEMPTY ($EMP_LEN file(s)):\n"
+  if [ "$EMP_LEN" != 0 ]
+  then
+    RED "\nEMPTY ($EMP_LEN file(s)):\n"
 
     for FILE in "${EMPTY_FILES[@]}"
     do 
-      printf "File "
-      RED "$FILE"
-      printf " is empty\n"
+      printf "$FILE\n"
     done
   fi
 
-  if [ "$UNEXP_LEN" != 0 ]; then
-    #RED "\nUNEXPECTED ($UNEXP_LEN file(s)):\n"
+  if [ "$UNEXP_LEN" != 0 ]
+  then
+    RED "\nUNEXPECTED ($UNEXP_LEN file(s)):\n"
 
-    for FILE in "${UNEXP_FILES[@]}"
+    for FILE in "${UNEXPECTED_FILES[@]}"
     do 
-      printf "File "
-      RED "$FILE"
-      printf " is unexpected\n"
+      printf "$FILE\n"
     done
   fi
     
   printf "$PIPE"
 
-  printf \
-  "Totally were found "$LOGS_LEN" file(s).
-  $FOUND_LEN of $CL_LEN needed files were found.
-  $OK_LEN of them are empty.
-  $NEF_LEN file(s) not found.
-  $UNEXP_LEN unexpected file(s) were found.\n"
+  
+  YELLOW "Totally were found : "; printf "$LOGS_LEN.\n"
+  YELLOW "Needed found : "; printf "$FOUND_LEN ""/"" $NEEDED_LEN.\n"
+  
+  if [[ "$EMP_LEN" != 0 ]]
+  then
+    printf "$EMP_LEN of them are "; YELLOW "empty.\n"
+  fi 
+
+  if [[ "$NEF_LEN" != 0 ]]
+  then 
+    YELLOW "Not found : "; printf "$NEF_LEN.\n"
+  fi 
+
+  if [[ "$UNEXP_LEN" != 0 ]]
+  then
+    YELLOW "Unexpected : "; printf "$UNEXP_LEN.\n" 
+  fi
+
+  printf "$PIPE"
+
 
   #UNMOUNT AND REMOVE DISK
-  sudo chown -R demid:users AMDZ_UNZIPPED_LOGS
   printf "Program completed\n"
   my_exit 0 2>/dev/null
 }
